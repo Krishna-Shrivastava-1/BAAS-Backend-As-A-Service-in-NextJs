01e1695df9404dbbd324182f7fef0b316ea668d7
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-11-11 20:49:54 +0530

    added somehting more

--- components/ProjectCards.jsx
+++ components/ProjectCards.jsx
@@ -3,10 +3,11 @@ import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Database, FolderKanban, KeyRound } from "lucide-react"
+import { Spinner } from './ui/spinner'
 
 const ProjectCards = () => {
   const [data, setData] = useState([])
-
+const [loading, setloading] = useState(true)
   const fetchProjects = async () => {
     try {
       const resp = await fetch(`/api/platform/project/getallproject`, {
@@ -16,6 +17,7 @@ const ProjectCards = () => {
       })
       const result = await resp.json()
       setData(result?.projectData || [])
+      setloading(false)
     } catch (error) {
       console.log(error)
     }
@@ -23,12 +25,20 @@ const ProjectCards = () => {
 
   useEffect(() => {
     fetchProjects()
-  }, [])
+  }, [loading])
 
-  if (!data.length) {
+  if (!data.length && !loading) {
     return (
       <div className="flex justify-center items-center h-48 text-muted-foreground">
         No projects found. Create your first project to get started.
+      </div>
+    )
+  }
+  if (loading) {
+    return (
+      <div className="flex justify-center gap-x-3 items-center h-48 text-muted-foreground">
+       <Spinner />
+       Loading...
       </div>
     )
   }
